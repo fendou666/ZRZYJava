04fbd60f987b736e06dfe07b8dd3cf5b508b0a4e
fendou666 <ls_code@126.com>
2017-03-20 20:25:24 +0800

    添加文件存储

---
+++ workspace/X2RFlowershop/src/com/chinasoft/po/Store.java
@@ -0,0 +1,24 @@
+package com.chinasoft.po;
+
+import java.io.Serializable;
+
+public class Store implements Serializable {
+
+	private static String DATA_PATH = "src/data.txt";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ArrayList<T> quertAllProducts(){
+		File file = new File(DATA_PATH);
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
