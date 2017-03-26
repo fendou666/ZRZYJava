489d93e3bc6f5de54121076babc0602711b539de
fendou666 <ls_code@126.com>
2017-03-27 07:19:57 +0800

    花店项目第一版

--- workspace/X2RFlowershop/src/com/chinasoft/po/Flower.java
+++ workspace/X2RFlowershop/src/com/chinasoft/po/Flower.java
@@ -28,7 +28,11 @@ public class Flower {
 	public Flower() {
 		super();
 	}
-
+//	用于delete花库存的时候用
+	public Flower(String name) {
+		this.name = name;
+	}
+	
 	public Flower(int number, String name, int count, double price) {
 		super();
 		this.number = number;
@@ -37,9 +41,6 @@ public class Flower {
 		this.price = price;
 	}
 
-	
-	
-	
 	
 	
 	public int getNumber() {
@@ -73,9 +74,37 @@ public class Flower {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	
-	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Flower other = (Flower) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "花编号：" +number + ", 花名：" + name + ", 当前库存："
+				+ count + ", 每束价格：" + price + "\n";
+	}
 	
 	
 	
